95f653585fea29833ffaf412fc2f4b767a5eaeb5
antti h <antti6789@live.com>
2018-01-03 16:47:43 +0200

    Some cleanup

--- src/app/classes/matrix.ts
+++ src/app/classes/matrix.ts
@@ -22,42 +22,29 @@ export class Matrix {
         }        
     }
 
-    matrixHasBeenSolved(  ) : boolean {
-        var tmp = true;
-        var cell : Cell;
-        for (let row = 0; row < 9; row++ ) {
-            for (let col = 0; col < 9; col++ ) {
-                cell = this.cells[row][col]
-                if (cell.pvals.length === 1 && cell.value === 0)
-                    tmp = false;
-            }
-        }        
-        return tmp;
-    }
-
-      initCellGroups(  ) {
+    initCellGroups(  ) {
         for ( let i = 1; i <= 9; i++ ) {
             this.rowGroups[i] = new RowGroup();
             this.colGroups[i] = new ColGroup();
             this.blockGroups[i] = new BlockGroup();
         }
-      }
+    }
 
-      addCellToCellGroups( cell : Cell ) {
+    addCellToCellGroups( cell : Cell ) {
         console.log("Adding cell to cell groups");
         this.rowGroups[cell.row].cells.push(cell);
         this.colGroups[cell.col].cells.push(cell);
         this.blockGroups[cell.block].cells.push(cell);
-      }
+    }
 
-      solve(){
+    solve(){
         for ( let i = 1; i <= 9; i++ ) {
             console.log("Block num: " + i);
             this.rowGroups[i].solve();
             this.colGroups[i].solve();
             this.blockGroups[i].solve();
         }
-      }
+    }
 
       
 }
